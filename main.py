5e217e399e1865ef9e0a7279cbceaf165f6d1cd3
Grzegorz Jędrzejczyk <gjedrzejczyk719@gmail.com>
2024-12-04 18:12:19 +0100

    Zmodyfikowano

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from datetime import time
 
-print("Hello git!")
+
 print(time.time())
 print("Test")
+print("Linijka1")
 #git config --global user.email "gjedrzejczyk719@gmail.com"
 #git config --global user.name "Grzegorz Jędrzejczyk"
